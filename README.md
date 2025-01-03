4c5d523733faa8e82a9a9b247559cec946bc1705
Droski0802 <pedromanuelwork@gmail.com>
2025-01-03 12:38:37 -0400

    Se agregaron los pasos para ejecutar el proyecto en el readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Topicos-Proyectico
+# BIENVENIDO A LA API DE CHISTES!
+# REQUERIMIENTOS: 1 - 2 - 7 ( PEDRO RODRIGUEZ )
+# REQUERIMIENTOS: ()
+# REQUERIMIENTOS: ()
+#
+#
+#
+# PASOS:
+#
+# 1- "npm install" - PARA INSTALAR LOS PAQUETES NECESARIOS
+#
+# 2- TENER MONGO INSTALADO 
+# 
+# 3 - CONECTARSE CON URI http://localhost:27017/jokes PARA VER LOS DATOS (SOLO si quieres ver los datos guardados)
+#
+# 4 - "npm start" - PARA EMPEZAR EL SERVIDOR Y EL PROYECTO!
+#
+# 5 - ABRIR TU "http://localhost:3000"
--- src/app.js
+++ src/app.js
@@ -1,23 +1,19 @@
-const express = require("express");
-const app = express();
-const path = require("path");
-const jokeRoutes = require("./routes/jokeRoutes");
+const express = require("express"); 
+const app = express(); 
+const path = require("path"); 
+const jokeRoutes = require("./routes/jokeRoutes"); 
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public"))); 
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "welcome.html"));
+app.get("/", (req, res) => { 
+  res.sendFile(path.join(__dirname, "public", "welcome.html")); 
 });
 
-app.use("/joke", jokeRoutes);
+app.use("/joke", jokeRoutes); 
 
-app.get("/joke", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "joke.html"));
-});
-
-app.get("/score", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "score.html"));
-});
+app.get("/joke", (req, res) => { 
+  res.sendFile(path.join(__dirname, "public", "joke.html")); 
+}); 
 
 module.exports = app;
